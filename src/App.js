82982a2f880934416d0c14acfb6b8958a43a2cf9
oklaM <675475355@qq.com>
2019-04-28 16:16:39 +0800

    a modify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Demo1 from './component/Demo1';
 
+
 function App() {
   return (
     <div className="App">
--- src/component/Demo1.js
+++ src/component/Demo1.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './Demo1.css';
-// import '../utils/demo1';
 import anime from 'animejs';
 import * as SVG from 'svg.js';
-
 
 class Demo1 extends Component{ 
     myRef = React.createRef();
     constructor(props) {
         super(props)
         this.state = {}
+        console.log(this)
     }
 
     render(){
@@ -18,12 +17,13 @@ class Demo1 extends Component{
                 <div className="wrapper"></div>
                 <button onClick={this.look}>look</button>
                 <button onClick={this.play}>play</button>
+                <button onClick={this.sync}>sync</button>
             </div>
             )    
     }
     
     componentDidMount() {
-        let drawing = SVG('container').width(500).height(500);
+        let drawing = SVG('container').width(800).height(500);
         let cx = drawing.cx();
 
         /* 神经网络 */
@@ -69,54 +69,169 @@ class Demo1 extends Component{
                 interPaths.push(path);
             }
         }
-        net.move(-300, 100);
+        net.addClass('net')
+        net.move(-200, 100);
         // let net2 = net.clone().move(100);
         // net.scale(0.5);
+
         /* ps */
         let ps = drawing.group();
         let psRect = ps.rect(100, 50).x(400).y(50).radius(5).fill('rgb(200, 255, 255)');
         let psText = ps.text('ps');
-        psText.x(psRect.cx() - 10).y(psRect.cy() - 10);
+        psText.x(psRect.cx() - psText.bbox().width / 2).y(psRect.cy() - psText.bbox().height / 2);
         ps.addClass('ps');
 
-        /* worker */
-        let worker = drawing.group();
-        let workerRect = worker.rect(100, 50).x(400).y(150).radius(5).fill('rgb(200, 255, 255)');
-        let workerText = worker.text('worker').x(workerRect.cx() - 30).y(workerRect.cy() - 10);
-		console.log("TCL: Demo1 -> componentDidMount -> workerText", workerText)
+        /* worker1 */
+        let worker1 = drawing.group();
+        let worker1Rect = worker1.rect(100, 50).x(400).y(150).radius(5).fill('rgb(200, 255, 255)');
+        let worker1Text = worker1.text('worker1')
+        worker1Text.x(worker1Rect.cx() - worker1Text.bbox().width / 2).y(worker1Rect.cy() - worker1Text.bbox().height / 2);
+        worker1.addClass('worker1');
+        worker1Rect.addClass('worker1Rect');
+
+        /* worker2 */
+        let worker2 = drawing.group();
+        let worker2Rect = worker2.rect(100, 50).x(400).y(230).radius(5).fill('rgb(200, 255, 255)');
+        let worker2Text = worker2.text('worker2')
+        worker2Text.x(worker2Rect.cx() - worker2Text.bbox().width / 2).y(worker2Rect.cy() - worker2Text.bbox().height / 2);
+        worker2.addClass('worker2');
+        worker2Rect.addClass('worker2Rect');
+            // let timeline = anime.timeline({
+            //     easing: 'easeOutExpo',
+            //     duration: 750
+            // })
+            // .add({
+            //     targets: 'net',
+            //     translateX: 250,
+            // });
         
-        let worker1 = worker.clone();
-        let worker2 = drawing.rect(100, 50).x(400).y(230).radius(5).fill('rgb(200, 255, 255)');
-        // let path = drawing.path("M 340,178 104,478 580,490 Z");
-        // let length = path.length();
-        // let rect = drawing.rect(5, 5)
-        // path.fill('none').stroke({width:1, color: '#ccc'}).move(10,10).scale(0.5)
-        // path.animate(3000).rotate(365).loop();
-        // firstNodes[0].animate(5000, '<>').during(function(pos, morph, eased){
-        //     var m = path.matrixify()
-        //     var p = new SVG.Point(path.pointAt(eased * length)).transform(m)
-        //     rect.move(p.x, p.y)
-        // }).loop(true, true)
-        net.addClass('net')
-        let timeline = anime.timeline({
-            easing: 'easeOutExpo',
-            duration: 750
-        })
-        .add({
-            targets: 'net',
-            translateX: 250,
-        });
+        
+        
         
         this.setState({
             drawing: drawing,
             net: net,
             ps: ps,
+            psRect: psRect,
             worker1: worker1,
-            worker2: worker2
+            worker1Rect: worker1Rect,
+            worker2: worker2,
+            worker2Rect: worker2Rect
         });
     }
 
     sync = () => {
+
+        const {drawing, ps, psRect, worker1Rect, worker2Rect} = this.state;
+        
+        let path1 = drawing.path(`
+            M ${worker2Rect.x()} ${worker2Rect.cy()}
+            C ${worker2Rect.x() - 80} ${worker2Rect.cy() - 100}, ${psRect.x()-80} ${psRect.cy()+100}, ${psRect.x()} ${psRect.cy()} 
+        `).fill('none').stroke({width: 1, color: '#BF7E96'}) 
+        path1.marker('end', 5, 5 )
+        path1.addClass('path1')
+
+        let path2 = drawing.path(`
+            M ${worker1Rect.x()} ${worker1Rect.cy()}
+            C ${worker1Rect.x() - 80} ${worker1Rect.cy() - 50}, ${psRect.x()-80} ${psRect.cy()+50}, ${psRect.x()} ${psRect.cy()} 
+        `).fill('none').stroke({width: 1, color: '#BF7E96'}) 
+        path2.marker('end', 5, 5 )
+        path2.addClass('path2')
+
+        let path3 = drawing.path(`
+            M ${psRect.x() + worker2Rect.width()} ${psRect.cy()} 
+            C ${worker2Rect.x() + worker2Rect.width()+ 80} ${worker2Rect.cy() - 100}, ${psRect.x()+ worker2Rect.width()+80} ${psRect.cy()+100}, ${worker2Rect.x() + worker2Rect.width()} ${worker2Rect.cy()}
+        `).fill('none').stroke({width: 1, color: '#BF7E96'}) 
+        path3.marker('end', 5, 5 )
+        path3.addClass('path3')
+
+        let path4 = drawing.path(`
+            M ${psRect.x() + worker2Rect.width()} ${psRect.cy()} 
+            C ${worker1Rect.x() + worker1Rect.width() + 80} ${worker1Rect.cy() - 50}, ${psRect.x() + worker1Rect.width()+80} ${psRect.cy()+50},${worker1Rect.x() + worker1Rect.width()} ${worker1Rect.cy()}
+        `).fill('none').stroke({width: 1, color: '#BF7E96'}) 
+        path4.marker('end', 5, 5 )
+        path4.addClass('path4')
+
+        let rect1 = drawing.rect(5,5)
+        rect1.addClass('rect1');
+        let rect2 = drawing.rect(5,5)
+        rect2.addClass('rect2');
+
+        let circle1 = drawing.circle(10).fill('#f02')
+        circle1.addClass('circle1');
+        let circle2 = drawing.circle(10).fill('#f02')
+        circle2.addClass('circle2');
+        
+        let path = anime.path('.path1')
+        anime({
+            targets: '.rect1',
+            translateX: path('x'),
+            translateY: path('y'),
+            rotate: path('angle'),
+            easing: 'linear',
+            duration: 2000,
+            loop: true
+          });
+        path = anime.path('.path2')
+        anime({
+            targets: '.rect2',
+            translateX: path('x'),
+            translateY: path('y'),
+            rotate: path('angle'),
+            easing: 'linear',
+            duration: 2000,
+            loop: true
+          });
+        path = anime.path('.path3')
+        anime({
+            targets: '.circle1',
+            translateX: path('x'),
+            translateY: path('y'),
+            rotate: path('angle'),
+            easing: 'linear',
+            duration: 2000,
+            loop: true
+        });
+        path = anime.path('.path4')
+        anime({
+            targets: '.circle2',
+            translateX: path('x'),
+            translateY: path('y'),
+            rotate: path('angle'),
+            easing: 'linear',
+            duration: 2000,
+            loop: true
+        });
+
+        var tl = anime.timeline({
+            loop: true
+        });
+        anime.set('.worker2Rect', {
+            width: 0
+            });
+        tl.add({
+            targets: '.worker2Rect',
+            width: 100, // -> from '28px' to '100%',
+            easing: 'easeInOutQuad',
+            // direction: 'alternate',
+            loop: true
+            });
+
+        anime.set('.worker1Rect', {
+            width: 0
+            });
+        tl.add({
+            targets: '.worker1Rect',
+            width: 100, // -> from '28px' to '100%',
+            easing: 'easeInOutQuad',
+            // direction: 'alternate',
+            loop: true
+            });
+
+        // worker2Rect.animate().attr({ width: 0 }).animate(2000, '>', 0).attr({ width: 100 , fill: '#f03'}).loop()
+    }
+
+    async = () => {
 
     }
 
